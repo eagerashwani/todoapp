a58e06ee4b5e52e5039dbad01064e60f73a58ea5
Ashwani Kumar <eagerashwani@gmail.com>
2022-11-07 10:06:51 +0530

    added

--- src/components/AllTask.jsx
+++ src/components/AllTask.jsx
@@ -1,8 +1,10 @@
+import Checkbox from "./Checkbox";
+
 export default function AllTask(){
     return (
         <div className="task">
-            <input type="checkbox" />
-            task name
+            <Checkbox defaultChecked={true} />
+            test task
         </div>
     );
 }
