448ed1b1cd05ff715ec7ba7cbdccefda0f24960f
Ashwani Kumar <eagerashwani@gmail.com>
2022-11-07 10:11:39 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Form from './components/Form'
 import './App.css';
 import AllTask from './components/AllTask';
+import { useState } from 'react';
 
-function App() {
+function App() { 
+  const [tasks, setTasks] = useState([]);
+  function addTask(task){
+    setTasks(prev => {
+      return [...prev, {task: task, done : false}];
+    })
+  }
   return (
    <main>
-    <Form />
+    <Form onAdd={addTask} />
     <AllTask />
    </main>
   );
